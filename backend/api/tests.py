50e7ff1a3d0fc4bc7ece6e626567fba3fa9d08e7
Светлана Хомякова <xoma_sveta@mail.ru>
2023-07-26 14:49:37 +0300

    test flake8

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,13 +1,11 @@
 # backend/api/tests.py
 from http import HTTPStatus
 
-from django.test import Client, TestCase
-
 from api import models
+from django.test import Client, TestCase
 
 
 class TaskiAPITestCase(TestCase):
-
     def setUp(self):
         self.guest_client = Client()
 
